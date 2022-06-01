54c94bd8f8069f458598618c8530ef78245fc473
Matheus <mporto1994@gmail.com>
2022-06-01 19:25:23 -0300

    feat: all done, too easy

--- hackaton.py
+++ hackaton.py
@@ -1,1 +1,9 @@
 # Desenvolva seu código abaixo
+hackathon_1 = ["Team Kenzie", "Team Ateliware", "Team VHSYS", "Team Mirum"]
+hackathon_2 = ["Team Ateliware", "Team Kenzie", "Team VHSYS", "Team Mirum"]
+hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
+
+def get_score(team_name,event):
+    return event.index(team_name)+1
+
+print(get_score("Team Mirum",hackathon_1))
