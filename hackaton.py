0c5a1f89609d149e4fb2112c742f4cde06368ebe
Matheus <mporto1994@gmail.com>
2022-06-11 15:35:34 -0300

    fix: gitingnore configurated and fixed the return of the function according to the business rules

--- hackaton.py
+++ hackaton.py
@@ -1,9 +1,7 @@
-# Desenvolva seu código abaixo
 hackathon_1 = ["Team Kenzie", "Team Ateliware", "Team VHSYS", "Team Mirum"]
 hackathon_2 = ["Team Ateliware", "Team Kenzie", "Team VHSYS", "Team Mirum"]
 hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
 
 def get_score(team_name,event):
-    return event.index(team_name)+1
-
-print(get_score("Team Mirum",hackathon_1))
+    posicao = event.index(team_name)+1
+    return f"A {team_name} foi classificada em {posicao} posição"
